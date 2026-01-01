99761d4dd0ed57e1792e5688484e4e2243ee02cb
Mohd Shadab <mohdshadab98977@gmail.com>
2026-01-02 00:06:44 +0530

    Remove node_modules from repository

---
+++ auth/server.js
@@ -0,0 +1,6 @@
+const app = require('./src/app');
+
+
+app.listen(5000,()=>{
+    console.log("Auth server is runnig on port 5000");
+})
---
+++ auth/src/app.js
@@ -0,0 +1,8 @@
+const exprees = require('express');
+const cookieparser = require('cookie-parser')
+
+
+const app = express();
+
+
+module.exports = app;
