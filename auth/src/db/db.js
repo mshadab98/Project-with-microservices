62f07a25059895b3f4e248a1c712b4afd380a289
mshadab98 <mohdshadab98977@gmail.com>
2026-01-02 14:24:08 +0530

    run the server setup

--- auth/src/db/db.js
+++ auth/src/db/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/auth');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to Auth DB");
     }catch(err){
         console.log("Error connecting to Auth DB", err);
